6ccdb6ce76a14bbeec59faae901f67580fc1fea6
Jordan <theghastlytuna@gmail.com>
2021-11-17 17:48:57 -0500

    Better dynamic waypoint tracking

--- Animation Demo/Assets/Scripts/FollowPath.cs
+++ Animation Demo/Assets/Scripts/FollowPath.cs
@@ -4,60 +4,87 @@ using UnityEngine;
 
 public class FollowPath : MonoBehaviour
 {
-    //for hard "coded" points
-    public Transform[] points;
-    public int currentPoint = 0;
+
     public float speed = 1;
+    public int currentPoint = 0;
     Rigidbody body;
-
-    //trying to get dynamic seeking
-    GameObject[] waypoints = GameObject.FindGameObjectsWithTag("Waypoint");
+    GameObject[] waypoints;
     GameObject closestWaypoint;
-
-    void Start()
+    GameObject targetWaypoint;
+    
+    private void ApproachPoint(GameObject target)
     {
-        body = this.GetComponent<Rigidbody>();
+        Vector3 direction = Vector3.Normalize(target.transform.position - body.gameObject.transform.position);
+        body.AddForce(direction * speed, ForceMode.Force);
     }
 
-    void Follow()
+    private int GetCurrentTargetIndex()
     {
-        //if we are close to a waypoint, increment the current target
-        if ((points[currentPoint].position - transform.position).magnitude > 2)
+        for (int i = 0; i < waypoints.Length; i++)
         {
-        Vector3 direction = Vector3.Normalize(points[currentPoint].position - transform.position);
-        body.AddForce(direction * speed, ForceMode.Force);
+            if (waypoints[i] == targetWaypoint)
+            {
+                return i;
+            }
+        }
+        return 0;
+    }
 
-        }
-        else
+    private int GetNextTargetIndex()
+    {
+        //loop through all waypoints
+        for (int i = 0; i < waypoints.Length; i++)
         {
-            if (currentPoint < 3)
+            //if we get the current target
+            if (waypoints[i] == targetWaypoint)
             {
-                currentPoint++;
-            }
-            else
-            {
-                //reset when at the end of the list
-                currentPoint = 0;
+                //if the current target is at the end of the array, return the start of the array
+                if (i == waypoints.Length - 1)
+                {
+                    return 0;
+                }
+                //return the next element of the array
+                return i + 1;
             }
         }
+        //if something messes up, return the start
+        return 0;
     }
 
     //check all the tagged waypoints for which is closest
-    void ScanClosestPoint()
+    private GameObject ScanClosestPoint()
     {
         foreach (var waypoint in waypoints)
         {
+            //distance from the waypoint to the cube
             float distance = (waypoint.transform.position - transform.position).magnitude;
-
-            if (distance > closestWaypoint.transform.position.magnitude)
+            //if new distance is less than current closest waypoint, set that as closest
+            if (distance < closestWaypoint.transform.position.magnitude && distance > 2)
             {
                 closestWaypoint = waypoint;
             }
         }
+        return closestWaypoint;
+    }
+    private void Start()
+    {
+        waypoints = GameObject.FindGameObjectsWithTag("Waypoint");
+        body = this.GetComponent<Rigidbody>();
+        closestWaypoint = waypoints[0];
+        targetWaypoint = ScanClosestPoint();
     }
 
-    void Update()
+    private void Update()
     {
-         Follow();
+        //if we are not at the target, move towards it
+        if ((targetWaypoint.transform.position - body.transform.position).magnitude > 2)
+        {
+            ApproachPoint(targetWaypoint);
+        }
+        //if we are at the target, go to the next
+        else
+        {
+            targetWaypoint = waypoints[GetNextTargetIndex()];
+        }
     }
 }
