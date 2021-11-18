b889d9d73b0d82ab9d4960a0a1564f6bf42a4071
Jordan <theghastlytuna@gmail.com>
2021-11-18 17:10:24 -0500

    Pain

--- Animation Demo/Assets/Scripts/FollowPath.cs
+++ Animation Demo/Assets/Scripts/FollowPath.cs
@@ -5,24 +5,18 @@ using UnityEngine;
 public class FollowPath : MonoBehaviour
 {
     public float speed = 1;
+    float t = 0;
     Rigidbody body;
     GameObject[] waypoints;
     GameObject closestWaypoint;
     GameObject targetWaypoint;
+    int targetIndex = 0;
     
-    private void ApproachPoint(GameObject target)
+    private int GetWaypointIndex(GameObject selection)
     {
-        Vector3 direction = Vector3.Normalize(target.transform.position - transform.position);
-        transform.forward = body.velocity.normalized;
-        body.AddForce(direction * speed, ForceMode.Force);
-    }
-
-    private int GetCurrentTargetIndex()
-    {
-        //probably don't need this
         for (int i = 0; i < waypoints.Length; i++)
         {
-            if (waypoints[i] == targetWaypoint)
+            if (waypoints[i].Equals(selection))
             {
                 return i;
             }
@@ -30,25 +24,70 @@ public class FollowPath : MonoBehaviour
         return 0;
     }
 
-    private int GetNextTargetIndex()
+    Vector3 GetCatmullRomPosition(float t, Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3)
     {
-        //loop through all waypoints
-        for (int i = 0; i < waypoints.Length; i++)
+        Vector3 a = 2f * p1;
+        Vector3 b = p2 - p0;
+        Vector3 c = 2f * p0 - 5f * p1 + 4f * p2 - p3;
+        Vector3 d = -p0 + 3f * p1 - 3f * p2 + p3;
+
+        Vector3 pos = 0.5f * (a + (b * t) + (c * t * t) + (d * t * t * t));
+
+        return pos;
+    }
+
+    void MoveOnCurve()
+    {
+        int p0 = 0, p1 = 0, p2 = 0, p3 = 0;
+
+        if (targetIndex == 0)// edge case if initial target ends up as the first element
         {
-            //if we get the current target
-            if (waypoints[i] == targetWaypoint)
-            {
-                //if the current target is at the end of the array, return the start of the array
-                if (i == waypoints.Length - 1)
-                {
-                    return 0;
-                }
-                //return the next element of the array
-                return i + 1;
-            }
+            p0 = waypoints.Length - 2;
+            p1 = waypoints.Length - 1;
+            p2 = targetIndex;
+            p3 = 1;
         }
-        //if something messes up, return the start
-        return 0;
+        else if (targetIndex == 1)// if moving from fist to second
+        {
+            p0 = waypoints.Length - 1;
+            p1 = 0;
+            p2 = targetIndex;
+            p3 = 2;
+        }
+        else if (targetIndex == waypoints.Length - 1)// if moving from second-last to last
+        {
+            p0 = targetIndex - 2;
+            p1 = targetIndex - 1;
+            p2 = targetIndex;
+            p3 = 0;
+
+        }
+        else if (targetIndex == waypoints.Length)// if trying to move past the last
+        {
+            p0 = waypoints.Length - 2;
+            p1 = waypoints.Length - 1;
+            p2 = 0;
+            p3 = 1;
+            targetIndex = 0;
+        }
+        else
+        {
+            p0 = targetIndex - 2;
+            p1 = targetIndex - 1;
+            p2 = targetIndex;
+            p3 = targetIndex + 1;
+        }
+
+        Vector3 curvePos = (GetCatmullRomPosition(
+            t,
+            waypoints[p0].transform.position,
+            waypoints[p1].transform.position,
+            waypoints[p2].transform.position,
+            waypoints[p3].transform.position));
+
+        Vector3 direction = curvePos - transform.position;
+
+        body.AddForce(direction * speed, ForceMode.Force);
     }
 
     //check all the tagged waypoints for which is closest
@@ -59,7 +98,7 @@ public class FollowPath : MonoBehaviour
             //distance from the waypoint to the cube
             float distance = (waypoint.transform.position - transform.position).magnitude;
             //if new distance is less than current closest waypoint, set that as closest
-            if (distance < closestWaypoint.transform.position.magnitude && distance > 2)
+            if (distance < (closestWaypoint.transform.position - transform.position).magnitude)
             {
                 closestWaypoint = waypoint;
             }
@@ -71,21 +110,23 @@ public class FollowPath : MonoBehaviour
         //fill the waypoint array, default the closest point to the first element, then scan through the array for the actual closest
         waypoints = GameObject.FindGameObjectsWithTag("Waypoint");
         body = this.GetComponent<Rigidbody>();
-        closestWaypoint = waypoints[0];
+        closestWaypoint = waypoints[1];
         targetWaypoint = ScanClosestPoint();
+        targetIndex = GetWaypointIndex(targetWaypoint) + 1;// +1 so that we start on the path going FROM the closest, not to the closest
     }
 
     private void Update()
     {
-        //if we are not at the target, move towards it
-        if ((targetWaypoint.transform.position - body.transform.position).magnitude > 2)
+        t += Time.deltaTime / 4;
+        if (t < 1)
         {
-            ApproachPoint(targetWaypoint);
+            MoveOnCurve();
+
         }
-        //if we are at the target, go to the next
         else
         {
-            targetWaypoint = waypoints[GetNextTargetIndex()];
+            targetIndex++;
+            t = 0;
         }
     }
 }
